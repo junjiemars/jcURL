a9ced18dbbeb71db0e1703fbbec20fcf2e1eb62d
南山 <junjiemars@gmail.com>
2015-04-30 16:25:14 +0800

    refine build.gradle

--- debug-web.sh
+++ debug-web.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 #export GRADLE_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
 ##gradlew :NHttpWeb:jettyRun &
+
+## just for tomcat7x
+export JPDA_OPTS='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005'
+
 gradlew --stacktrace :NHttpWeb:cargoRunLocal &
