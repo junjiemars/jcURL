3a7aa99b323a95ae32d172deff11c26e6e95643b
南山 <junjiemars@gmail.com>
2015-05-17 17:36:18 +0800

    play around spring-boot for web

---
+++ web/src/main/java/com/xw/http/web/Core.java
@@ -0,0 +1,16 @@
+package com.xw.http.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author:junjie
+ * Create:17/5/15.
+ * Target:<>
+ */
+@SpringBootApplication
+public class Core {
+    public static void main(String[] args) {
+        SpringApplication.run(Core.class, args);
+    }
+}
---
+++ web/src/main/java/com/xw/http/web/GreetingController.java
@@ -0,0 +1,22 @@
+package com.xw.http.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Author:junjie
+ * Create:17/5/15.
+ * Target:<>
+ */
+@Controller
+public class GreetingController {
+    @RequestMapping("/greeting")
+    public String greeting(
+            @RequestParam(value="name", required = false, defaultValue = "World") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
